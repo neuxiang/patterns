3f8a4953534d7ce37546fa556201ccd33cc44bef
neuxiang <neuxiang@126.com>
2014-09-14 18:59:14 +0800

    test

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,7 +6,7 @@ public class TestClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String filePathName = "a.a.a.mp3";
+		String filePathName = "a.a.mp3";
 		String extName=filePathName.substring( filePathName.lastIndexOf("."),filePathName.length());
 		System.out.print(extName);
 		System.out.print(extName.equalsIgnoreCase(".Mp3"));
